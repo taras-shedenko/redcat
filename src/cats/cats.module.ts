881cecc297dc0de398a3d97748c67f98178730ed
Taras Shedenko <taras.shedenko@gmail.com>
2026-01-03 15:09:44 +0200

    cats module

---
+++ src/cats/cats.module.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+import { Module } from '@nestjs/common';
+import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
+
+@Module({
+  controllers: [CatsController],
+  providers: [CatsService],
+  exports: [CatsService],
+})
+export class CatsModule {}
